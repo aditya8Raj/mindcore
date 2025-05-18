0ace479b27fd00d562c260e584400757b88b341c
Aditya Raj <aditya7raj1@gmail.com>
2025-05-18 11:48:24 +0530

    added google fonts but they are not working

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  fontFamily: {
+    font1: ["Space Grotesk", "sans-serif"],
+    font2: ["DM Sans", "sans-serif"],
+    font3: ["IBM Plex Sans", "sans-serif"],
+  },
 };
 
 export default config;
