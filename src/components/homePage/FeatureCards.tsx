512f64d4af7362fde963263dafe5b5fe59b8efcd
Aditya Raj <aditya7raj1@gmail.com>
2025-05-18 11:48:54 +0530

    making features card

--- src/components/homePage/FeatureCards.tsx
+++ src/components/homePage/FeatureCards.tsx
@@ -4,7 +4,7 @@ const FeatureCards = () => {
   return (
     <section className="pb-12">
       <div className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-3xl md:text-4xl font-bold text-white mb-8">
+        <h2 className="text-3xl md:text-4xl font-bold text-white pb-16 font-font2">
           Tools That Supercharge Your AI Workflow
         </h2>
         {/* ----- cards here ------ */}
